7bc53a6a68a00da45b65adb33fa8e1f4f5a802ae
ecssiah <ecssiah@gmail.com>
2019-02-04 20:32:10 -0600

    Add icons to bottom tab navigator

--- src/containers/MainNavigator.js
+++ src/containers/MainNavigator.js
@@ -1,20 +1,51 @@
 import React from "react";
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import { createBottomTabNavigator, createAppContainer } from "react-navigation";
 
 import FocusesScreen from '../components/FocusesScreen';
 import StatsScreen from '../components/StatsScreen';
 import SettingsScreen from '../components/SettingsScreen';
 
-const MainNavigator = createBottomTabNavigator({
-  Stats: {
-    screen: StatsScreen
-  }, 
-  Focuses: {
-    screen: FocusesScreen
+import { Colors } from '../styles';
+
+const MainNavigator = createBottomTabNavigator(
+  {
+    Stats: {
+      screen: StatsScreen
+    },
+    Focuses: {
+      screen: FocusesScreen
+    },
+    Settings: {
+      screen: SettingsScreen
+    },
   },
-  Settings: {
-    screen: SettingsScreen
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+
+        if (focused) tintColor = '#ffffff';
+
+        if (routeName === 'Focuses') {
+          iconName = 'ios-eye';
+        } else if (routeName === 'Settings') {
+          iconName = 'md-settings';
+        } else if (routeName === 'Stats') {
+          iconName = 'ios-podium';
+        }
+
+        return <Ionicons name={iconName} size={35} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      showLabel: false,
+      style: {
+        backgroundColor: Colors.statusBar,
+      },
+    },
   },
-});
+);
 
 export default createAppContainer(MainNavigator);
