5f3665e54fd4b5cb7f331c432d76418c07f023d5
ecssiah <ecssiah@gmail.com>
2019-02-08 21:48:37 -0600

    Update LoginScreen with submit button

--- src/components/LoginScreen/index.js
+++ src/components/LoginScreen/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TextInput } from 'react-native';
+import { Button, View, Text, TextInput } from 'react-native';
 import styles from './style';
 
 class LoginScreen extends React.Component {
@@ -12,19 +12,44 @@ class LoginScreen extends React.Component {
     };
   }
 
+  onPressLogin = () => {
+    this.setState({
+      username: 'Testing',
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
+        <Text
+          style={styles.section}
+        >
+          Username
+        </Text>
+
         <TextInput
           style={{height: 40, width: 120, borderColor: 'gray', borderWidth: 1}}
           onChangeText={username => this.setState({username})}
           value={this.state.username}
         />
+
+        <Text
+          style={styles.section}
+        >
+          Password
+        </Text>
+
         <TextInput
           secureTextEntry={true}
           style={{height: 40, width: 120, borderColor: 'gray', borderWidth: 1}}
           onChangeText={password => this.setState({password})}
           value={this.state.password}
+        />
+
+        <Button
+          onPress={this.onPressLogin}
+          title="Login"
+          color="#841584"
         />
       </View>
     );
