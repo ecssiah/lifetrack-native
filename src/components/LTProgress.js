a15d75f561d385a335f836964751e47b92a54052
ecssiah <ecssiah@gmail.com>
2019-03-03 20:42:38 -0600

    Add skeleton for LTProgress

---
+++ src/components/LTProgress.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Image, View } from 'react-native';
+import createStyles from '../styles';
+
+const styles = createStyles({
+  container: {
+  },
+  backgroundWrapper: {
+  },
+  backgroundImage: {
+  },
+  progressWrapper: {
+  },
+  progressImage: {
+  }, 
+});
+
+class LTProgress extends React.Component {
+  render() {
+    return (
+      <View style={styles.container} >
+        <View style={styles.backgroundWrapper} >
+          <Image 
+            style={styles.backgroundImage} 
+            source={require('../../assets/textures/progress-back.png')}
+          />
+        </View>
+
+        <View style={styles.progressWrapper} >
+          <Image 
+            style={styles.progressImage} 
+            source={require('../../assets/textures/progress-bar.png')}
+          />
+        </View>
+      </View>
+    );
+  };
+};
+
+export default LTProgress;
