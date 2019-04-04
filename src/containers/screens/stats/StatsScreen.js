d1afa8edac6034d69e7fa8c6489feedcbfce9ee9
ecssiah <ecssiah@gmail.com>
2019-04-04 09:40:36 -0500

    Freeze before removing untracked

--- src/containers/screens/stats/StatsScreen.js
+++ src/containers/screens/stats/StatsScreen.js
@@ -3,12 +3,9 @@ import { connect } from 'react-redux'
 import { View } from 'react-native'
 import { StackedAreaChart, Grid, XAxis } from 'react-native-svg-charts'
 import * as shape from 'd3-shape'
-import { getUniqueColors, getToday } from '../../../../lib/utils';
+import { getUniqueColors } from '../../../../lib/utils';
 import { LIFE_EXPECTANCY, SECONDS_IN_YEAR } from '../../../constants/User'
 import createStyles from '../../../styles'
-
-import LTText from '../../../components/LT/LTText'
-import LTSpacer from '../../../components/LT/LTSpacer'
 
 const styles = createStyles({
   container: {
@@ -26,49 +23,6 @@ class StatsScreen extends React.Component
 {
   static navigationOptions = {
     title: 'Stats',
-  }
-
-
-  _formatUntrackedTime(time) {
-    const days = Math.floor(time / 60 / 60 / 24)
-    const hours = Math.floor((time / 60 / 60) - days * 24)
-    const minutes = Math.floor((time / 60) - (days * 24 * 60) - (hours * 60))
-    const seconds = Math.floor(
-      time - (days * 24 * 60 * 60) - (hours * 60 * 60) - (minutes * 60)
-    )
-
-    const displayMinutes = minutes < 10 ? '' + minutes : minutes
-    const displaySeconds = seconds < 10 ? '0' + seconds : seconds
-
-    let fullTime = ''
-
-    if (days > 1) {
-      fullTime += days + ' days, '
-    } else if (days > 0) {
-      fullTime += days + ' day, '
-    }
-
-    if (hours > 0) {
-      fullTime += `${hours}:${displayMinutes}:${displaySeconds}`
-    } else {
-      fullTime += `${minutes}:${displaySeconds}`
-    }
-
-    return fullTime
-  }
-
-
-  _getUntrackedLifePercentage() {
-    if (isNaN(this.props.user.birthYear)) {
-      return 'Requires Birth Year'
-    } else {
-      const currentYear = new Date().getFullYear()
-      const userAge = currentYear - parseInt(this.props.user.birthYear)
-      const secondsLeft = (LIFE_EXPECTANCY - userAge) * SECONDS_IN_YEAR
-      const percentageUsed = this.props.stats.untracked / secondsLeft 
-
-      return percentageUsed.toFixed(2) + '%'
-    }
   }
 
 
@@ -90,7 +44,7 @@ class StatsScreen extends React.Component
 
   _getMainChartData() {
     const data = []
-    const dateRange = this._getDateRange('March 31, 2019', 'April 4, 2019') 
+    const dateRange = this._getDateRange('March 30, 2019', 'April 4, 2019') 
 
     for (const focusKey of Object.keys(this.props.focuses)) {
       const focus = this.props.focuses[focusKey]
@@ -128,6 +82,19 @@ class StatsScreen extends React.Component
     return colors
   }
 
+  _getMainChartSvgs() {
+    const keys = [...new Set(Object.keys(this.props.focuses))]
+    const svgs = []
+
+    for (let key of keys) {
+      svgs.push({ 
+        onPress: () => console.warn(this.props.focuses[key].name)
+      })
+    }
+
+    return svgs
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -137,6 +104,7 @@ class StatsScreen extends React.Component
           data={this._getMainChartData()}
           keys={this._getMainChartKeys()}
           colors={this._getMainChartColors()}
+          svgs={this._getMainChartSvgs()}
           curve={shape.curveLinear}
           showGrid={true}
         >
@@ -158,21 +126,3 @@ const mapDispatchToProps = dispatch => ({
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(StatsScreen)
-
-        // <LTText style={styles.mainText}>
-        //   Untracked Time: 
-        // </LTText>
-          
-        // <LTText style={styles.mainText}>
-        //   {this._formatUntrackedTime(this.props.stats.untracked)}
-        // </LTText>
-
-        // <LTSpacer large />
-
-        // <LTText style={styles.mainText}>
-        //   Untracked Percentage: 
-        // </LTText>
-          
-        // <LTText style={styles.mainText}>
-        //   {this._getUntrackedLifePercentage()}
-        // </LTText>
