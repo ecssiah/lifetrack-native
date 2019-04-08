fa156adf8bd6f8082b9f7a3bf1878e42f36bd3d3
ecssiah <ecssiah@gmail.com>
2019-04-08 15:59:39 -0500

    Move animate prop to VictoryStack

--- src/components/stats/StatsChart.js
+++ src/components/stats/StatsChart.js
@@ -60,7 +60,6 @@ class StatsChart extends React.Component
           domainPadding={8}
           padding={{ top: 12, bottom: 34, left: 12, right: 12 }}
           maxDomain={{ y: SECONDS_IN_DAY }}
-          animate={{ duration: 1000 }}
         >
           <VictoryAxis
             tickValues={this.props.dates}
@@ -76,7 +75,10 @@ class StatsChart extends React.Component
             }}
           />
 
-          <VictoryStack colorScale={this.props.colors} >
+          <VictoryStack 
+            colorScale={this.props.colors} 
+            animate={{ duration: 1000 }}
+          >
             {this._getMainChartStack()}
           </VictoryStack>
         </VictoryChart>
--- src/containers/screens/help/HelpScreen.js
+++ src/containers/screens/help/HelpScreen.js
@@ -1,11 +1,54 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { View } from 'react-native'
-import createStyles, { FontSize } from '../../../styles'
+import { ScrollView, View } from 'react-native'
+import Ionicon from 'react-native-vector-icons/Ionicons'
+import MaterialCommunityIcon 
+from 'react-native-vector-icons/MaterialCommunityIcons'
+import createStyles, { FontSize, Color } from '../../../styles'
 
 import LTIcon from '../../../components/LT/LTIcon';
+import LTText from '../../../components/LT/LTText';
 
 const styles = createStyles({
+  container: {
+    flex: 1,
+  },
+  focusesSection: {
+
+  },
+  focusesHeader: {
+    flexDirection: 'row',
+    margin: 4,
+    backgroundColor: Color.secondary,
+  },
+  focusesTitle: {
+    fontSize: 22,
+    color: '#ffffff',
+  },
+  statsSection: {
+
+  },
+  statsHeader: {
+    flexDirection: 'row',
+    margin: 4,
+    backgroundColor: Color.secondary,
+  },
+  statsTitle: {
+    fontSize: 22,
+    color: '#ffffff',
+  },
+  settingsSection: {
+
+  },
+  settingsHeader: {
+    flexDirection: 'row',
+    margin: 4,
+    backgroundColor: Color.secondary,
+  },
+  settingsTitle: {
+    fontSize: 22,
+    color: '#ffffff',
+  },
 })
 
 class HelpScreen extends React.Component 
@@ -25,6 +68,53 @@ class HelpScreen extends React.Component
   render() {
     return (
       <View style={styles.container}>
+        <ScrollView>
+          <View style={styles.focusesSection}>
+            <View style={styles.focusesHeader} >
+              <MaterialCommunityIcon 
+                name={'target'} 
+                size={32} 
+                color={'#ffffff'} 
+              />
+
+              <LTText style={styles.focusesTitle}>
+                Focuses
+              </LTText>
+
+            </View>
+
+          </View>
+
+          <View style={styles.statsSection}>
+            <View style={styles.statsHeader} >
+              <Ionicon 
+                name={'ios-podium'} 
+                size={35} 
+                color={'#ffffff'} 
+              />
+
+              <LTText style={styles.statsTitle}>
+                Stats
+              </LTText>
+
+            </View>
+          </View>
+
+          <View style={styles.settingsSection}>
+            <View style={styles.settingsHeader} >
+              <Ionicon 
+                name={'md-settings'} 
+                size={35} 
+                color={'#ffffff'} 
+              />
+
+              <LTText style={styles.settingsTitle}>
+                Settings
+              </LTText>
+
+            </View>
+          </View>
+        </ScrollView>
       </View>
     )
   }
